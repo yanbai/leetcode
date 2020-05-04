b01fe34d7ee2ab307a1e2a38630951108184b6cd
yanbai <ilovestevenzhang@gmail.com>
2020-05-04 10:28:49 +0800

    added 155.min stack

---
+++ 155.min-stack.js
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=155 lang=javascript
+ *
+ * [155] Min Stack
+ */
+
+// @lc code=start
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.data  = []
+    this.orderStack = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    this.data[this.data.length] = x
+    if(this.orderStack.length === 0) {
+        this.orderStack.push(x)
+    } else {
+        if(x <= this.orderStack[this.orderStack.length-1]) {
+            this.orderStack.push(x)
+        }
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    let topItem = this.data.pop()
+    if(topItem === this.orderStack[this.orderStack.length-1]) {
+        this.orderStack.pop()
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.data[this.data.length - 1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.orderStack[this.orderStack.length-1]
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+var obj = new MinStack()
+obj.push(-2)
+obj.push(-3)
+obj.push(0)
+
+// obj.pop()
+// var param_3 = obj.top()
+var param_4 = obj.getMin()
+// @lc code=end
+
